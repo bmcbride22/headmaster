92e903af144d193c13d00b52675185613a55592f
Brendan McBride <brendan.mcmath@gmail.com>
2023-01-07 14:28:16 +0800

    feat: add unit_assessment join table model and related associations

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: assessments
+#
+#  id         :bigint           not null, primary key
+#  date       :date
+#  title      :string
+#  type       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  subject_id :bigint           not null
+#
+# Indexes
+#
+#  index_assessments_on_subject_id  (subject_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (subject_id => subjects.id)
+#
 class Assessment < ApplicationRecord
   belongs_to :subject
+  has_many :unit_assessments
+  has_many :units, through: :unit_assessments
 end
--- test/models/assessment_test.rb
+++ test/models/assessment_test.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: assessments
+#
+#  id         :bigint           not null, primary key
+#  date       :date
+#  title      :string
+#  type       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  subject_id :bigint           not null
+#
+# Indexes
+#
+#  index_assessments_on_subject_id  (subject_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (subject_id => subjects.id)
+#
 require "test_helper"
 
 class AssessmentTest < ActiveSupport::TestCase
